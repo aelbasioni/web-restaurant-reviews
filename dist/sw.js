c0d0edfb915678ca88948c53708dc06385f48396
Asmaa <a.elbasioni@gmail.com>
2018-05-19 21:19:32 +0200

    refactor: restructure the project folders for task automations
    	- organize the files in src & dist folders
    	- add npm package.json
    	- add gulpfile.js
    	- manage js, css, html minifications, concat, sourcemap, and babel

---
+++ dist/sw.js
@@ -0,0 +1,135 @@
+const STATIC_CACHE_NAME = 'restaurants-static-v1';
+const CONTENT_IMAGES_CACHE = 'restaurants-content-imgs';
+const ALL_CACHES = [
+  STATIC_CACHE_NAME,
+  CONTENT_IMAGES_CACHE
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open(STATIC_CACHE_NAME).then(function (cache) {
+          return cache.addAll([
+            '/',
+            '/restaurant.html',
+            'js/main.js',
+            'js/dbhelper.js',
+            'js/restaurant_info.js',
+            'js/localforage.min.js',
+            'css/styles.css',
+            'img/logo.png',
+            'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+            'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+          ]);
+      })
+    );
+});
+
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName.startsWith('restaurants-') &&
+                 !ALL_CACHES.includes(cacheName);
+        }).map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+
+self.addEventListener('fetch', (event) => {
+    const requestUrl = new URL(event.request.url);
+    if (requestUrl.origin === location.origin) {        
+        if (requestUrl.pathname === '/restaurant.html') {
+            event.respondWith(caches.match('/restaurant.html'));
+            return;
+        }
+
+        if (requestUrl.pathname.startsWith('/img/')) {
+            event.respondWith(serveImage(event.request));
+            return;
+        }
+    }
+
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+          return response || fetch(event.request);
+      })
+    );
+});
+
+
+function serveImage(request) {
+    var storageUrl = request.url.replace(/-\d+px\.jpg$/, '');
+    
+    return caches.open(CONTENT_IMAGES_CACHE).then((cache) => {
+        return cache.match(storageUrl).then((response) => {
+            if (response) return response;
+
+            return fetch(request).then((networkResponse) => {
+                cache.put(storageUrl, networkResponse.clone());
+                return networkResponse;
+            });
+        });
+    });
+}
+
+
+/*
+(function () {
+
+    const CACHE_NAME = 'static-cache-v1';
+    const urlsToCache = [
+      '.',
+      'index.html',
+      'restaurant.html',
+      'css/styles.css',
+      'data/restaurants.json',
+      'js/dbhelper.js',
+      'js/main.js',
+      'js/restaurant_info.js',
+      'js/localforage.min.js'
+    ];
+
+    self.addEventListener('install', (event) => {
+        event.waitUntil(
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+        );
+    });
+
+    self.addEventListener('fetch', (event) => {
+        event.respondWith(
+          caches.match(event.request)
+            .then(response => {
+                return response || fetchAndCache(event.request);
+            })
+        );
+    });
+
+    function fetchAndCache(url) {
+        return fetch(url)
+          .then(response => {
+              console.log(response, url);
+              if (!response.ok) {
+                  throw Error(response.statusText);
+                  //Promise.reject();
+              }
+
+              return caches.open(CACHE_NAME)
+                .then(cache => {
+                    cache.put(url, response.clone());
+                    return response;
+                })
+          })
+          //.catch(error => {
+          //    console.log('Request failed:', error);
+          //    return;
+          //});
+    }
+
+})();*/
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require("gulp");
+var htmlclean = require('gulp-htmlclean');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+
+var config = {
+    //Include all js files but exclude any min.js files
+    src: {
+        //js: ['./restaurant/js/*.js', '!./restaurant/js/*.min.js'],
+        js_index: ['./restaurant/js/localForage.min.js', './restaurant/js/dbhelper.js', './restaurant/js/main.js'],
+        js_restaurant_info: ['./restaurant/js/localForage.min.js', './restaurant/js/dbhelper.js', './restaurant/js/restaurant_info.js'],
+        css: ['./restaurant/css/*.css', '!./restaurant/css/*.min.css'],
+        img: './restaurant/img/*.jpg',
+        html: './restaurant/*.html',
+        root: './restaurant/'
+    },
+    dist: {
+        js: './dist/js/',
+        css: './dist/css/',
+        img: './dist/img/',
+        html: './dist/',
+        root: './dist/',
+    }
+}
+
+gulp.task('copy', function () {
+    //gulp.src(config.src.root + '/web.config').pipe(gulp.dest(config.dist.root)),
+    gulp.src(config.src.root + '/sw.js').pipe(gulp.dest(config.dist.root)),
+    gulp.src(config.src.root + 'data/*.json').pipe(gulp.dest(config.dist.root + 'data/'))
+    
+});
+
+gulp.task('html:dist', function () {
+    return gulp.src(config.src.html)
+        .pipe(plumber())      
+        .pipe(htmlclean())
+        .pipe(gulp.dest(config.dist.html));
+});
+
+gulp.task('css:dist', function () {
+    return gulp.src(config.src.css)
+        .pipe(plumber())
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(config.dist.css));
+});
+
+
+gulp.task('js_index:dist', function () {
+    return gulp.src(config.src.js_index)
+        .pipe(plumber())
+        //.pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(concat('script_index.min.js'))
+        .pipe(uglify())
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.dist.js));
+});
+
+gulp.task('js_info:dist', function () {
+    return gulp.src(config.src.js_restaurant_info)
+        .pipe(plumber())
+        //.pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(concat('script_info.min.js'))
+        .pipe(uglify())
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.dist.js));
+});
+
+gulp.task('copy:dist', ['html:dist', 'css:dist', 'js_index:dist', 'js_info:dist', 'copy']);
+
+
+gulp.task('watch', function () {
+    gulp.watch([config.src.html], ['html:dist']);
+    gulp.watch([config.src.css], ['css:dist']);
+    gulp.watch([config.src.js_index], ['js_index:dist']);
+    gulp.watch([config.src.js_restaurant_info], ['js_info:dist']);
+    //gulp.watch([config.src.root + '/web.config', config.src.root + '/sw.js', 'data/*.json'], ['copy']);
+    gulp.watch([config.src.root + '/sw.js', 'data/*.json'], ['copy']);
+});
+
+//Set a default tasks
+gulp.task('default', ['copy:dist','watch'], function () {
+    // place code for your default task here
+});
